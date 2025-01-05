3f86278715fe8cf1283a818a34df47f4aba4b524
Bryan Wu <wzb3422@gmail.com>
2025-01-05 22:51:21 +0800

    feat: wip check session

---
+++ server/utils/access-token.ts
@@ -0,0 +1,35 @@
+import { appid, appSecret, jwtSecret } from '~/constants';
+import qs from 'querystring'
+import dayjs from 'dayjs';
+import { handleMpError } from './mp-error';
+
+let accessToken = ''
+let expirationTimestamp = 0
+
+const isExpired = () => {
+    const now = dayjs().unix()
+    return now > expirationTimestamp
+}
+
+export const getAccessToken = async () => {
+    if (accessToken && !isExpired()) {
+        return accessToken
+    }
+
+    const url = 'https://api.weixin.qq.com/cgi-bin/token';
+    const res = await fetch(`${url}?${qs.stringify({
+        appid,
+        secret: appSecret,
+        grant_type: 'client_credential'
+    })}`, {
+        method: 'GET'
+    })
+    const resObj = await res.json()
+
+    handleMpError(resObj)
+
+    accessToken = resObj.access_token
+    expirationTimestamp = dayjs().unix() + resObj.expires_in - 180 // 提前 3 分钟刷新 access_token
+
+    return accessToken
+}
